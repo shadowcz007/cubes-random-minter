a4d2e447c64682e6c4ef36ab68883a12cf88d84f
shadow <chizhiwei007@163.com>
2022-03-31 14:41:49 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 /** Connect to Moralis server */
 const serverUrl = "https://5ip9ljtq95ri.usemoralis.com:2053/server";
 const appId = "FLIwJ8FTlPyVQ0bRrTomHvILmrkddeGRYTB9RuIz";
@@ -41,10 +42,13 @@ async function login() {
   }
 }
 
+
+
 function initApp(){
     document.querySelector("#app").style.display = "flex";
     document.querySelector("#submit_button").onclick = submit;
-    document.querySelector("#input_refresh").onclick = runAnimate;
+    // document.querySelector("#input_refresh").onclick = runAnimate;
+    document.querySelector("#target").onclick = runAnimate;
 
     // your id
     const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
@@ -66,12 +70,13 @@ function initApp(){
       });
 
       window.drawingCanvas=document.createElement( 'canvas' );
-
+      
       create();
 
 };
 
 async function submit(){
+   
     let message=document.querySelector('#success_message');
     message.innerHTML = `- ipfs - init`;
     message.style.display = "block";
@@ -114,10 +119,61 @@ async function submit(){
     document.querySelector('#success_message').style.display = "block";
     // setTimeout(() => {
     //     document.querySelector('#success_message').style.display = "none";
-    // }, 5000)
+        
+    // }, 1000)
 }
 
 window.cubes=[];
+
+function createGeometry() {
+
+				const geometry = new THREE.BoxGeometry(1,1, 1, 32, 32, 32 );
+
+				// create an empty array to  hold targets for the attribute we want to morph
+				// morphing positions and normals is supported
+				geometry.morphAttributes.position = [];
+
+				// the original positions of the cube's vertices
+				const positionAttribute = geometry.attributes.position;
+
+				// for the first morph target we'll move the cube's vertices onto the surface of a sphere
+				const spherePositions = [];
+
+				// for the second morph target, we'll twist the cubes vertices
+				const twistPositions = [];
+				const direction = new THREE.Vector3( 1, 0, 0 );
+				const vertex = new THREE.Vector3();
+
+				for ( let i = 0; i < positionAttribute.count; i ++ ) {
+
+					const x = positionAttribute.getX( i );
+					const y = positionAttribute.getY( i );
+					const z = positionAttribute.getZ( i );
+
+					spherePositions.push(
+
+						x * Math.sqrt( 1 - ( y * y / 2 ) - ( z * z / 2 ) + ( y * y * z * z / 3 ) ),
+						y * Math.sqrt( 1 - ( z * z / 2 ) - ( x * x / 2 ) + ( z * z * x * x / 3 ) ),
+						z * Math.sqrt( 1 - ( x * x / 2 ) - ( y * y / 2 ) + ( x * x * y * y / 3 ) )
+
+					);
+
+					// stretch along the x-axis so we can see the twist better
+					vertex.set( x * 2, y, z );
+
+					vertex.applyAxisAngle( direction, Math.PI * x / 2 ).toArray( twistPositions, twistPositions.length );
+
+				}
+
+				// add the spherical positions as the first morph target
+				geometry.morphAttributes.position[ 0 ] = new THREE.Float32BufferAttribute( spherePositions, 3 );
+
+				// add the twisted positions as the second morph target
+				geometry.morphAttributes.position[ 1 ] = new THREE.Float32BufferAttribute( twistPositions, 3 );
+
+				return geometry;
+
+			}
 
 function create(){
       const scene = new THREE.Scene();
@@ -126,6 +182,13 @@ function create(){
       let canvas=document.querySelector('#target'),
       width=parseInt(getComputedStyle(document.querySelector('#target')).width),
       height=parseInt(getComputedStyle(document.querySelector('#target')).height);
+
+      let mouseX = 0, mouseY = 0;
+
+      // 画布的中点
+			let windowHalfX =  width/2 +canvas.offsetLeft;
+			let windowHalfY =  height/2 +canvas.offsetTop;
+      // console.log(windowHalfX,windowHalfY)
       // console.log(document.querySelector('#target').style.width)
       // canvas.width=width;
       // canvas.height=height;
@@ -142,7 +205,7 @@ function create(){
 			// renderer.setSize( window.innerWidth, window.innerHeight );
 			// document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.BoxGeometry();
+			const geometry = createGeometry();
       const material=new THREE.MeshPhongMaterial( { color: 0x808080, dithering: true } );
 			// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 			const cube = new THREE.Mesh( geometry, material );
@@ -150,7 +213,7 @@ function create(){
 			// scene.add( cube );
 
       // clone
-      for (let i = -10; i <10; i++) {
+      for (let i = -100; i <100; i++) {
        let x=cube.position.x+(i+1)*2;
         for (let index = -10; index < 10; index++) {
            let c=cube.clone();
@@ -192,21 +255,51 @@ function create(){
 				light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
 				scene.add( light4 );
         
+        scene.add( new THREE.AmbientLight( 0x8FBCD4, 0.4 ) );
 
 			camera.position.z = 5;
 
-			function animate() {
-        requestAnimationFrame( animate );
+      function onPointerMove( event ) {
+				if ( event.isPrimary === false ) return;
+        // console.log(event.clientX,event.clientY)
+         mouseX = event.clientX - windowHalfX;
+				 mouseY = event.clientY - windowHalfY;
+         
+        //  控制在画布范围内
+        // closeChangeCamera=!!(mouseX>=width/2||mouseX<-width/2||mouseY>=height/2||mouseY<-height/2)
+         
+        // if(mouseX<0) mouseX=Math.min(mouseX,-width/2);
+        // if(mouseY>=0) mouseY=Math.max(mouseY,height/2);
+        // if(mouseY<0) mouseY=Math.min(mouseY,-height/2);
+        // console.log(mouseX,mouseY)
+			}
+
+      function onWindowResize() {
+				windowHalfX =  width/2 +canvas.offsetLeft;
+			  windowHalfY =  height/2 +canvas.offsetTop;
+				// camera.aspect = window.innerWidth / window.innerHeight;
+				// camera.updateProjectionMatrix();
+				// renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+
+      function render(){
         // console.log('animate')
-        if(!window.isDone){
-             for (const cube of window.cubes) {
+        if(!window.animateIsFinish){
+          
+          let count=30;
+            for (const cube of window.cubes) {
               cube.rotation.x += 0.01*Math.random();
               cube.rotation.y += 0.01*Math.random();
+              // cube.position.z = Math.random()>0.5?-0.3:0;
+              if(count>0){
+                //cube.scale.x += Math.random()*(Math.random()>0.5?-0.02:0.02);
+                // Spherify
+                cube.morphTargetInfluences[ 0 ] = window.spherify
+                // Twist
+                cube.morphTargetInfluences[ 1 ] =window.twist;
+                count--;
+              };
             }
-            
-
-            renderer.render( scene, camera );
-
             const time = Date.now() * 0.0025;
 
             light1.position.x = Math.sin( time * 0.7 ) * 30;
@@ -224,25 +317,58 @@ function create(){
             light4.position.x = Math.sin( time * 0.3 ) * 30;
             light4.position.y = Math.cos( time * 0.7 ) * 40;
             light4.position.z = Math.sin( time * 0.5 ) * 30;
-            if(!window.check){
-              setTimeout(()=>window.isDone=true,3000);
-              window.check=true;
+            if(!window.checkAnimate){
+              setTimeout(()=>window.animateIsFinish=true,3500+(3000*Math.random()));
+              window.checkAnimate=true;
             }
             
+            camera.position.x += ( mouseX - camera.position.x ) * 0.0001;
+            camera.position.y += ( - mouseY - camera.position.y ) * 0.0001;
+            camera.rotation.x += ( mouseX - camera.rotation.x ) * 0.000001;
+            camera.rotation.y += ( - mouseY - camera.rotation.y ) * 0.000001;
         }
-				
-       
+
+				// camera.lookAt( scene.position );
+      // console.log(camera.position.x,camera.position.y)
+        renderer.render( scene, camera );
+
+      }
+			function animate() {
+        requestAnimationFrame( animate );
+        render();
 			};
+
+
+
+      document.body.style.touchAction = 'none';
+			document.body.addEventListener( 'pointermove', onPointerMove );
+      window.addEventListener( 'resize', onWindowResize );
 
 			animate();
 
       window.renderer=renderer;
       window.scene=scene;
+      window.camera=camera;
 };
 
+
+
 function runAnimate(){
-  window.isDone=false;
-  window.check=false;
+  window.animateIsFinish?window.animateIsFinish=false:null;
+  window.checkAnimate?window.checkAnimate=false:null;
+  window.spherify=Math.random()*1.2;
+  window.twist=Math.random()*1.2;
+  window.cubes=shuffle(window.cubes);
+  // 打乱
+  function shuffle (arr) {
+    let arrNew = [...arr]
+    //用Math.random()函数生成0~1之间的随机数与0.5比较，返回-1或1
+    const randomsort = function (a, b) {
+      return Math.random() > 0.5 ? -1 : 1
+    }
+    // var arr = [1, 2, 3, 4, 5];
+    return [...arrNew.sort(randomsort)]
+  }
 }
 
 function drawText(){
@@ -268,3 +394,7 @@ function getTargetResult(){
 
 login();
 
+
+
+// https://threejs.org/examples/?q=mor#webgl_morphtargets
+// 
